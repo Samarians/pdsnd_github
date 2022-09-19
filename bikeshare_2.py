5ac6a7de0e6d21ba785809365cc6879435e54122
Samarians <nguyendinhnhan0901@gmail.com>
2022-09-19 12:49:14 +0700

    Update single/double quotes to main function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -229,7 +229,7 @@ def main():
         user_stats(df)
         show_raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input("\nWould you like to restart? Enter 'yes' or 'no'.\n")
         if restart.lower() != 'yes':
             break
 
